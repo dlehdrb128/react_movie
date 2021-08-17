6073f145f22643bef1a23220b05a0d09f584b35f
MacBook Pro 15 <macbookpro15@MacBookui-MacBookPro.local>
2021-08-18 01:58:22 +0900

    props 사용법과 구조분해

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import React from 'react'
 
+const Food = ({fav}) => {
+  return <h1>i like {fav}</h1>
+}
 
 function App() {
   return (
     <div>
-      hellow!!!
+     <h1>hellow!</h1>
+      <Food fav="kimchi" />
+      <Food fav="라면"/>
+      <Food fav="햄버거" />
     </div>
   );
 }
