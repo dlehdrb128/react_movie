d84880828bee0f647f03966dc93dfdec457239c5
MacBook Pro 15 <macbookpro15@MacBookui-MacBookPro.local>
2021-08-18 11:56:39 +0900

    리액트 라이프 사이클

--- src/App.js
+++ src/App.js
@@ -2,22 +2,22 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 class App extends React.Component {
-  state = {
-    count: 0
+ state = {
+   isLoading: true
+ };
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({isLoading: false})
+    }, 6000);
   }
  render() {
-   return <div>
-     <h1>The number is: {this.state.count}</h1>
-     <button onClick={this.add}>add</button>
-     <button onClick={this.minus}>minus</button>
-     </div>
+    const {isLoading} = this.state;
+   return (
+     <div>{isLoading ? "Loading...": "we are ready"}</div>
+   )
+     
  }
- add = () => {
-   this.setState(current => ({count: current.count + 1}))
- };
- minus = () => {
-  this.setState({count: -1})
- }
+ 
 }
 
 
